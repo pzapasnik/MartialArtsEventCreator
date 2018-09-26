c2c384938ab65a90685a3abeac8805c274a0ff3d
pzapasnik <pzapasnik@gmail.com>
2018-09-26 21:42:27 +0200

    FighterDetailsRepository correct

--- src/main/java/pl/coderslab/martial_arts_event_creator_app/Repository/FighterDetailsRepository.java
+++ src/main/java/pl/coderslab/martial_arts_event_creator_app/Repository/FighterDetailsRepository.java
@@ -1,7 +1,8 @@
 package pl.coderslab.martial_arts_event_creator_app.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.coderslab.martial_arts_event_creator_app.Model.User.FighterDetails;
 
-public interface FighterDetailsRepository extends JpaRepository<FighterDetailsRepository, Long> {
+public interface FighterDetailsRepository extends JpaRepository<FighterDetails, Long> {
 
 }
