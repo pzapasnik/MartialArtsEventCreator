1cd34cdcbe955d309324e53cdebdd2fdfc2063b0
pzapasnik <pzapasnik@gmail.com>
2018-09-26 21:39:06 +0200

    added FighterDetailsRepository

---
+++ src/main/java/pl/coderslab/martial_arts_event_creator_app/Repository/FighterDetails.java
@@ -0,0 +1,7 @@
+package pl.coderslab.martial_arts_event_creator_app.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FighterDetails extends JpaRepository<FighterDetails, Long> {
+    
+}
